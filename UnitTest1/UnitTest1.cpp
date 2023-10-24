84d06fb9b7e196cac97505d642b7f30f4df4fc81
N0ONE25 <valvladgonext@gmail.com>
2023-10-24 20:30:55 +0300

    5.0(1)

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,30 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../5.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1) //правильно
+		{
+			double a = 2;
+			double b = 3;
+			double exres = -0.0214935;
+			double acres = g(a, b);
+			Assert::AreEqual(exres, acres, 1e-7);
+		}
+		TEST_METHOD(TestMethod2) //не правильно
+		{
+			double a = 2;
+			double b = 3;
+			double exres = -0.021674935;
+			double acres = g(a, b);
+			Assert::AreEqual(exres, acres, 1e-7);
+		}
+	};
+}
